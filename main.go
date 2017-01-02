c8de8123522204f9f6ac7d5bb9cb106194a98d00
carloscarvallo <carloscarvallo87@gmail.com>
2017-01-02 17:08:23 -0300

    fix sync with channel msg

--- main.go
+++ main.go
@@ -19,8 +19,8 @@ var cb = coinbase.ApiKeyClient(os.Getenv("CB_API_KEY"), os.Getenv("CB_SECRET_KEY
 var config = oauth1.NewConfig(os.Getenv("TW_API_KEY"), os.Getenv("TW_API_SECRET"))
 var token = oauth1.NewToken(os.Getenv("TW_ACCESS_TOKEN"), os.Getenv("TW_TOKEN_SECRET"))
 var httpClient = config.Client(oauth1.NoContext, token)
-var fTicketHour = "00:00"
-var sTicketHour = "12:00"
+var fTicketHour = "17:05"
+var sTicketHour = "17:06"
 
 func formatCommas(num int) string {
 	numString := strconv.Itoa(num)
@@ -48,10 +48,10 @@ func getExchange(c chan bool, x chan string) {
 }
 
 func tweetCurrency(xMsg chan string) {
+	exchangePYG := <-xMsg
 	client := twitter.NewClient(httpClient)
 	currentTime := time.Now().Local()
 	timeFormatted := currentTime.Format("2006-01-02 15:04:05")
-	exchangePYG := <-xMsg
 	_, resp, sendErr := client.Statuses.Update(timeFormatted+"\n1 BTC son: "+exchangePYG+"Gs. #btc #btcXpyg #guaranies #py #bitcoin", nil)
 	fmt.Println("Tweeted at ", timeFormatted)
 	fmt.Println("Resp ", resp)
